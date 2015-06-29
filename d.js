0eacecbd421ab67423de4d6647080e20fd1fe2c1
laem <l4em@outlook.com>
2015-06-29 22:07:47 +0200

    offset updated, more precise

--- d.js
+++ d.js
@@ -2,7 +2,6 @@ var fs = require('fs')
 var items = require('./mongoimport.json')
 
 //48.844800, 2.383192 48.844812, 2.383289
-
 var p1 = [2.383075, 48.844875]
 var p2 = [2.383708, 48.844931]
 var q1 = [2.383192, 48.844800]
@@ -11,10 +10,16 @@ var diff = {
 	long: p1[0] - p2[0] + q1[0] - q2[0],
 	lat: p1[1] - p2[1] + q1[1] - q2[1]
 }
+// diff.long : -0.00073
+// diff.lat : -0.000068
 
 function transformCoords(coords){
-	return [coords[0] + diff.long, coords[1] + diff.lat]
+	//more precise values than above
+	return [coords[0] - 0.000724, coords[1] - 0.0000685]
 }
+
+
+
 items.map(function(item){
 	var g = item.geometry,
 	coordList = g.coordinates;
